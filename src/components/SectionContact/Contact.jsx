91c1d02a8a4a478b09ddc430ce2ee8ee1c8647fc
avicdro <avicdro@gmail.com>
2019-04-26 21:10:02 +0200

    3er

--- src/components/SectionContact/Contact.jsx
+++ src/components/SectionContact/Contact.jsx
@@ -16,7 +16,6 @@ export default class Contact extends Component {
     return (
       <div className={styles.containerSection}>
         <div className={styles.titleContact}>
-          {/* <h3>¿Trabajamos Juntos?</h3> */}
           <p>
             Si quieres comunicarte conmigo, siéntete libre de hacerlo a través 
             de alguna de mis redes sociales. 
@@ -24,7 +23,7 @@ export default class Contact extends Component {
         </div>
         <div className={styles.containerRed}>
           <SocialNetwork name="Linkedin" href="https://www.linkedin.com/in/victor-alvarez-tallada-152563141/" />
-          <SocialNetwork name="twitter" href="https://twitter.com/@avicdro" />          
+          <SocialNetwork name="twitter" href="https://twitter.com/avicdro" />          
           <SocialNetwork name="GitHub" href="https://github.com/avicdro" />
           <SocialNetwork name="Mail" href="https://avicdro@gmail.com" />          
         </div>
